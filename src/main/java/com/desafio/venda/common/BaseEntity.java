8b120b98a44d25f40c1982dc666a7e5128bc3eb6
Iuri Guerreiro <bentoiuri@gmail.com>
2026-03-26 16:02:47 -0300

    refactor: padroniza IDs das entidades usando BaseEntity

---
+++ src/main/java/com/desafio/venda/common/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.desafio.venda.common;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
--- src/main/java/com/desafio/venda/model/Venda.java
+++ src/main/java/com/desafio/venda/model/Venda.java
@@ -1,40 +1,38 @@
 package com.desafio.venda.model;
 
+import com.desafio.venda.common.BaseEntity;
 import com.desafio.venda.dto.VendaRequest;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "venda")
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Venda {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+public class Venda extends BaseEntity {
 
     @Column(name = "data_venda", nullable = false)
     private LocalDateTime dataVenda;
     @Column(name = "valor", nullable = false)
     private BigDecimal valor;
 
-    @Column(name = "vendedor")
-    private Vendedor vendedor;
+    @Column(name = "vendedor_id", nullable = false)
+    private Integer vendedorId;
+    @Column(name = "vendedor_nome", nullable = false)
+    private String vendedorNome;
 
     public static Venda convert(VendaRequest vendaRequest){
         return Venda.builder()
-                .dataVenda(LocalDateTime.now())
                 .valor(vendaRequest.getValor())
-                .vendedor(vendaRequest.getVendedorId())
+                .dataVenda(LocalDateTime.now())
+                .vendedorId(vendaRequest.getVendedorId())
+                .vendedorNome(vendaRequest.getVendedorNome())
                 .build();
     }
 }
